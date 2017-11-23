b8bb6d2eeb8d44b4e54fd49a90b117f1a9ca8f5e
LP Umum <umum@lp.if.its.ac.id>
2017-11-23 12:48:14 +0700

    stuck lagi :(

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import './App.css';
 import Artikel from "./Artikel";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Artikel/>
-        <p>test</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">CrawlTest</h1>
+                </header>
+                <Artikel/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Artikel.js
+++ src/Artikel.js
@@ -1,55 +1,71 @@
 import React, { Component } from 'react';
 const request = require('request');
 const cheerio = require('cheerio');
-//const crawler = require('crawler');
+const max = 5;
+
+let obj = new Array(max);
 
 class Artikel extends Component {
     constructor(props) {
         super(props);
+        //let cn = [];
         this.state = {
             content: undefined,
         };
+
+        function getContent(uri, i, thou) {
+            const update = (error, response, body) => {
+                const $ = cheerio.load(body);
+                obj.conten = $('#review-body').children('p').text();
+                console.log(i, obj.conten);
+                const c = obj.map((obj) =>
+                    <div>
+                        <h3><a href={obj.link}>{obj.title}</a></h3>
+                        <p>{obj.conten}</p>
+                    </div>
+                );
+                console.log(c.toString());
+                thou.state.content = c;
+            };
+            request.get({url: uri}, update);
+        }
+
         const updateContent = (error, response, body) => {
             console.log(body);
-            this.setState({content: body});
-            const $ = cheerio.load(this.state.content);
+            //this.setState({content: body});
+            const $ = cheerio.load(body);
             const lnk = $('.news-item');
             let i = 0;
-            console.log('Headlines of: www.gsmarena.com');
+            //let links = [];
+            //console.log(c);
             lnk.each(function(){
-                if(i <= 5){
-                    console.log(i + '.', $(this).children('a').children('h3').text());
-                    console.log('http://www.gsmarena.com/' + $(this).children('a').attr('href'));
-                    console.log();
+                if(i < max){
+                    obj[i] = {
+                        title : $(this).children('a').children('h3').text(),
+                        link : 'http://www.gsmarena.com/' + $(this).children('a').attr('href'),
+                    };
                     i++;
                 }
             });
+            for(i = 0; i < max; i++){
+                obj.conten = getContent(('http://www.gsmarena.com/' + $(lnk[i]).children('a').attr('href')), i, this);
+            }
+            const c = obj.map((obj) =>
+                <div>
+                    <h3><a href={obj.link}>{obj.title}</a></h3>
+                    <p>{obj.conten}</p>
+                </div>
+            );
+            this.setState({content: c});
         };
         request.get({url: 'https://www.gsmarena.com/'}, updateContent);
-
-        console.log(this.state.content);
-        /*if (this.state.content) {
-            const $ = cheerio.load(this.state.content);
-        } else {
-            console.log('Cheerio Error');
-        }*/
-        /*const lnk = $('.news-item');
-        var i = 0;
-        console.log('Headlines of: www.gsmarena.com')
-        lnk.each(function(){
-            if(i <= 5){
-                console.log(i + '.', $(this).children('a').children('h3').text());
-                console.log('http://www.gsmarena.com/' + $(this).children('a').attr('href'));
-                console.log();
-                i++;
-            }
-        });*/
     }
 
     render() {
         return (
-            <div>
-                <p>{}</p>
+            <div className="article-body">
+                <h1>'Headlines of: www.gsmarena.com'</h1>
+                {this.state.content}
             </div>
         );
     }

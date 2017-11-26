b61309de11f42577fb7f6fc84958b9489d96bb54
Aditya Pramana <mirror.ojoakua@gmail.com>
2017-11-26 19:10:46 +0700

    tinggal moles frontend + (mungkin kalau sempat coba crawl situs lain)

--- src/Artikel.js
+++ src/Artikel.js
@@ -1,71 +1,67 @@
 import React, { Component } from 'react';
 const request = require('request');
 const cheerio = require('cheerio');
-const max = 5;
+const max = 7;
 
-let obj = new Array(max);
 
 class Artikel extends Component {
     constructor(props) {
         super(props);
         //let cn = [];
         this.state = {
-            content: undefined,
+            content: [],
         };
+
+        let obj = new Array(max);
 
         function getContent(uri, i, thou) {
             const update = (error, response, body) => {
                 const $ = cheerio.load(body);
-                obj.conten = $('#review-body').children('p').text();
-                console.log(i, obj.conten);
-                const c = obj.map((obj) =>
-                    <div>
-                        <h3><a href={obj.link}>{obj.title}</a></h3>
-                        <p>{obj.conten}</p>
-                    </div>
-                );
-                console.log(c.toString());
-                thou.state.content = c;
+                const k = $('#review-body').children('p').text().split(" ");
+                for(let j = 0; j < 20; j++){
+                    obj[i].conten += (k[j] + " ");
+                }
+                thou.setState({content : obj});
             };
             request.get({url: uri}, update);
         }
 
         const updateContent = (error, response, body) => {
             console.log(body);
-            //this.setState({content: body});
             const $ = cheerio.load(body);
             const lnk = $('.news-item');
             let i = 0;
-            //let links = [];
-            //console.log(c);
             lnk.each(function(){
                 if(i < max){
                     obj[i] = {
                         title : $(this).children('a').children('h3').text(),
                         link : 'http://www.gsmarena.com/' + $(this).children('a').attr('href'),
+                        conten : '',
+                        imguri : $(this).children('a').children('div').children('img').attr('src'),
                     };
                     i++;
                 }
             });
             for(i = 0; i < max; i++){
-                obj.conten = getContent(('http://www.gsmarena.com/' + $(lnk[i]).children('a').attr('href')), i, this);
+                getContent(('http://www.gsmarena.com/' + $(lnk[i]).children('a').attr('href')), i, this);
             }
-            const c = obj.map((obj) =>
-                <div>
-                    <h3><a href={obj.link}>{obj.title}</a></h3>
-                    <p>{obj.conten}</p>
-                </div>
-            );
-            this.setState({content: c});
         };
         request.get({url: 'https://www.gsmarena.com/'}, updateContent);
     }
 
     render() {
+        let cn = this.state.content.map((obj) =>
+            <div key={obj.title}>
+                <h3><a href={obj.link}>{obj.title}</a></h3>
+                <img src={obj.imguri} alt={obj.title}/>
+                <p>{obj.conten}<a href={obj.link}>Read more...</a></p>
+            </div>
+        );
+
         return (
             <div className="article-body">
-                <h1>'Headlines of: www.gsmarena.com'</h1>
-                {this.state.content}
+                <h1>Top {max} Headlines of: www.gsmarena.com</h1>
+                {cn}
             </div>
         );
     }
